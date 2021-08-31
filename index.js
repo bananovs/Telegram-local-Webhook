21c97a1dacf8cf82de858c42b36ef4e92c1c5426
bananovs <r.s.bazhanov@gmail.com>
2021-08-31 22:44:59 +0300

    v2

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 const Telegraf = require('telegraf');
 const request = require('request');
 const util = require('util');
-var config = require('./config.json');
+var myArgs = process.argv.slice(2);
+var config = require(myArgs[0]);
 
 // Создаем объект бота
 const bot = new Telegraf(config.token)
 .use((ctx) => { // вешаем прослушку
+    // console.log(util.inspect(ctx.update.callback_query.data, false, null, true /* enable colors */))
     console.log(util.inspect(ctx.update, false, null, true /* enable colors */))
     // при событии отправляем обновление на наш обработчик
     request({ 
